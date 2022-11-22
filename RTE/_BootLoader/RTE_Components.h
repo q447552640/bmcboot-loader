e32bdcb99f58aaa101e8e8c06c2ac8a1962a3891
马玉琛 <JimMa0312@outlook.com>
2022-11-22 21:46:49 +0800

    Create BMC BootLoader Application Git repository

---
+++ RTE/_BootLoader/RTE_Components.h
@@ -0,0 +1,31 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'BootLoader' 
+ * Target:  'BootLoader' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "gd32f4xx.h"
+
+/* GigaDevice::Device:StdPeripherals:GPIO:1.0.3 */
+#define RTE_DEVICE_STDPERIPHERALS_GPIO
+/* GigaDevice::Device:StdPeripherals:MISC:1.0.3 */
+#define RTE_DEVICE_STDPERIPHERALS_MISC
+/* GigaDevice::Device:StdPeripherals:PMU:1.0.3 */
+#define RTE_DEVICE_STDPERIPHERALS_PMU
+/* GigaDevice::Device:StdPeripherals:RCU:1.0.3 */
+#define RTE_DEVICE_STDPERIPHERALS_RCU
+/* GigaDevice::Device:StdPeripherals:USART:1.0.3 */
+#define RTE_DEVICE_STDPERIPHERALS_USART
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ bsp_uart.h
@@ -0,0 +1,12 @@
+/*
+ * @Author: Ma Yuchen
+ * @Date: 2022-11-22 21:43:50
+ * @LastEditors: Ma YuChen
+ * @LastEditTime: 2022-11-22 21:44:48
+ * @Description: file content
+ * @FilePath: \BootLoader\bsp_uart.h
+ */
+#ifndef BSP_UART_H
+#define BSP_UART_H
+
+#endif
