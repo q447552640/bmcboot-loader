3976ea65df49a3b81a1313878479a496b87c752a
马玉琛 <JimMa0312@outlook.com>
2022-11-26 14:36:52 +0800

    add 450 Eval Target item

---
+++ RTE/_BootLoaderEval/RTE_Components.h
@@ -0,0 +1,33 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'BootLoader' 
+ * Target:  'BootLoaderEval' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "gd32f4xx.h"
+
+/* GigaDevice::Device:StdPeripherals:FMC:1.0.3 */
+#define RTE_DEVICE_STDPERIPHERALS_FMC
+/* GigaDevice::Device:StdPeripherals:GPIO:1.0.3 */
+#define RTE_DEVICE_STDPERIPHERALS_GPIO
+/* GigaDevice::Device:StdPeripherals:MISC:1.0.3 */
+#define RTE_DEVICE_STDPERIPHERALS_MISC
+/* GigaDevice::Device:StdPeripherals:PMU:1.0.3 */
+#define RTE_DEVICE_STDPERIPHERALS_PMU
+/* GigaDevice::Device:StdPeripherals:RCU:1.0.3 */
+#define RTE_DEVICE_STDPERIPHERALS_RCU
+/* GigaDevice::Device:StdPeripherals:USART:1.0.3 */
+#define RTE_DEVICE_STDPERIPHERALS_USART
+
+
+#endif /* RTE_COMPONENTS_H */
